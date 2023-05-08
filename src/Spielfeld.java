a0e245d6fedd41c417c6be1d6b8121b12861f460
Leon Feder <122815625+LeonFeder07@users.noreply.github.com>
2023-05-08 15:39:50 +0200

    .
    .

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -1,6 +1,6 @@
 import GLOOP.*;
 public class Spielfeld{
-    private GLQuader untergrund;
+    private GLQuader untergrund,rechts,links,oben,unten;
     private double breite, tiefe;
 
     public Spielfeld(double pBreite, double pTiefe){
@@ -8,5 +8,15 @@ public class Spielfeld{
         tiefe = pTiefe;
         untergrund = new GLQuader(0,0,0, breite,20,tiefe);
         untergrund.setzeTextur("src/img/Marmor.jpg");
+        rechts = new GLQuader(breite/2,20,0, 10,20,tiefe);
+        links = new GLQuader(breite/(-2),20,0, 10,20,tiefe);
+        unten = new GLQuader(0,20,tiefe/(-2), breite,20,10);
+        oben = new GLQuader(0,20,tiefe/(2), breite,20,10);
+    }
+
+    public double getBreite() {
+        return breite;
+    } public double getTiefe() {
+        return tiefe;
     }
 }
